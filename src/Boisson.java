a06ecf4507bce1af0e8492ee78cff4e9c3fa3698
AdrienKamdem <64603216+AdrienKamdem@users.noreply.github.com>
2021-09-29 23:55:04 +0200

    Add files via upload

---
+++ src/Boisson.java
@@ -0,0 +1,18 @@
+public class Boisson extends BoissonMere {
+	
+
+	public Boisson(String nom, float contenance, float prix) {
+		super(nom, contenance, prix);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	public String toStringDispo() {
+		return super.toStringDispo();
+	}
+		
+}
